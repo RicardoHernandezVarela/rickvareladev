dede9a78e0f41f304e88eaf8ba9156b923120355
Rick Varela <rickvarela@MacBook-Air-de-ricardo.local>
2025-11-15 20:17:56 -0600

    inlinecards

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,9 +1,20 @@
 import { Flex, Text } from "@chakra-ui/react";
 
+import InlineCards from "@/src/sections/InlineCards";
+
+const styles = {
+  mainContainer: {
+    w: "100%",
+    maxW: "1650px",
+    h: "100vh",
+    alignItems: "center",
+  },
+};
+
 function ProjectsPage() {
   return (
-    <Flex>
-      <Text>ProjectsPage</Text>
+    <Flex {...styles.mainContainer} direction="column" overflowY="scroll">
+      <InlineCards />
     </Flex>
   );
 }
